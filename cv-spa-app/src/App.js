0774da644695ca102e08cd4548f8ee06df86d79d
tokj <tkjelds385@gmail.com>
2023-12-17 13:11:34 +0100

    addded dark mode toggle

--- cv-spa-app/src/App.js
+++ cv-spa-app/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import useLocalStorage from 'use-local-storage'
+import React from "react";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
 
 function App() {
+  const [theme, setTheme] = useLocalStorage('theme' ? true : false)
+
+  const switchTheme = (checked)  => {
+    setTheme(checked)
+  }
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme? 'light' : 'dark'}>
+      <div className='TopBar'>
+      <DarkModeSwitch
+      className='DarkModeToggle'
+      style={{ marginBottom: '2rem' }}
+      checked={theme}
+      onChange={switchTheme}
+      size={40}
+    />
+      </div>
+      <div className='Content'>
       <h1>Hello World!</h1>
+      </div>
+      <div className='BottomBar'>
+
+      </div>
+
     </div>
   );
 }
