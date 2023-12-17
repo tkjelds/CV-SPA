720f2cc525124b367dacfd8f699c1c15a1be8962
tokj <tkjelds385@gmail.com>
2023-12-17 15:06:34 +0100

    added localiaztion support

--- cv-spa-app/src/App.js
+++ cv-spa-app/src/App.js
@@ -1,35 +1,46 @@
 import './App.css';
 import useLocalStorage from 'use-local-storage'
-import React from "react";
+import React, { Suspense } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { I18nContext, useTranslation } from 'react-i18next';
 
 
 function App() {
-  const [theme, setTheme] = useLocalStorage('theme' ? true : false)
+  const { t, i18n } = useTranslation();
+
+  const [lng, setLng] = useLocalStorage("lng" ? true : false) // true = dk, false = en
+
+  const [theme, setTheme] = useLocalStorage('theme' ? true : false) // true = light, false = dark
 
   const switchTheme = (checked)  => {
     setTheme(checked)
   }
 
+  const changeLanguage = () => {
+    setLng(!lng);
+    lng ? i18n.changeLanguage("dk") : i18n.changeLanguage("en");
+  };
+
   return (
-    <div className="App" data-theme={theme? 'light' : 'dark'}>
-      <div className='TopBar'>
-      <DarkModeSwitch
-      className='DarkModeToggle'
-      style={{ marginBottom: '2rem' }}
-      checked={theme}
-      onChange={switchTheme}
-      size={40}
-    />
+      <div className="App" data-theme={theme? 'dark' : 'light'}>
+        <div className='TopBar'>
+          <DarkModeSwitch
+          className='DarkModeToggle'
+          style={{ marginBottom: '2rem' }}
+          checked={theme}
+          onChange={switchTheme}
+          size={40}
+          />
+        <button className='Language_button' type="button" onClick={() => changeLanguage()}>
+          {lng ? "EN" : "DK"}
+        </button>
+        </div>
+        <div className='Content'>
+        <h1 className='Title'>{t('title')}</h1>
+        </div>
+        <div className='BottomBar'>
+        </div>
       </div>
-      <div className='Content'>
-      <h1>Hello World!</h1>
-      </div>
-      <div className='BottomBar'>
-
-      </div>
-
-    </div>
   );
 }
 
