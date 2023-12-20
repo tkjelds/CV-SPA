58d59c881b6d2fe92b2746608842f893c390413d
tokj <tkjelds385@gmail.com>
2023-12-20 11:56:15 +0100

    first cleanup

--- cv-spa-app/src/App.js
+++ cv-spa-app/src/App.js
@@ -11,8 +11,8 @@ function App() {
   const [lng, setLng] = useLocalStorage("lng" ? true : false) // true = dk, false = en
 
   const [theme, setTheme] = useLocalStorage('theme' ? true : false) // true = light, false = dark
-  
-  const [sectionContent, setSectionContent] = useState("section-text-about-me")
+
+  const [sectionContent, setSectionContent] = useState("lorem-ipsum-long")
 
   const [sectionTitle, setSectionTitle] = useState('selection-title-about-me')
 
@@ -24,25 +24,24 @@ function App() {
     setSectionContent(sectionContent)
   }
 
-  function updateSectionProjects(selectionTitle, sectionContent){
+  function updateSectionProjects(selectionTitle, sectionContent) {
     setContentSwitch(false)
     setSectionTitle(selectionTitle);
     setTextContent(sectionContent)
   }
 
-  const switchTheme = (checked)  => {
+  const switchTheme = (checked) => {
     setTheme(checked)
   }
 
   const changeLanguage = () => {
     setLng(!lng);
-    updateSection(sectionTitle,sectionContent)
+    updateSection(sectionTitle, sectionContent)
     lng ? i18n.changeLanguage("dk") : i18n.changeLanguage("en");
   };
 
-  function section()
-  {              
-    return(
+  function section() {
+    return (
       <div className='section'>
         <h2 className='section-title'>{t(sectionTitle)}</h2>
         <span className='section-content'>
@@ -54,58 +53,55 @@ function App() {
 
   return (
     <Suspense >
-      <div className="app" data-theme={theme? 'dark' : 'light'}>
+      <div className="app" data-theme={theme ? 'dark' : 'light'}>
         <div className='header'>
           <DarkModeSwitch
-          className='DarkModeToggle'
-          style={{ marginBottom: '2rem' }}
-          checked={theme}
-          onChange={switchTheme}
-          size={40}
+            className='DarkModeToggle'
+            style={{ marginBottom: '2rem' }}
+            checked={theme}
+            onChange={switchTheme}
+            size={40}
           />
-        <button className='Language_button' type="button" onClick={() => changeLanguage()}>
-          {lng ? "EN" : "DK"}
-        </button>
+          <button className='Language_button' type="button" onClick={() => changeLanguage()}>
+            {lng ? "EN" : "DK"}
+          </button>
         </div>
         <div className='main-container'>
-          <div className='main-container-inner'>
-            <div className='inner-container'>
-              <h1 className='Title'>{t('title')}</h1>
-              <div className='Title-spacer'></div>
-              <div className='selector-content'>
-                <h2 className='selector-title'>{t(sectionTitle)}</h2>
-                <div className='selection-wrapper'>
-                  <div className='selection'>
-                    <a>
-                      <span onClick={() => updateSection("selection-title-about-me","section-text-about-me")}>
-                        {t('selection-title-about-me')}
-                      </span>
-                    </a>
-                    <a>
-                      <span onClick={() => updateSection("selection-title-projects","section-text-projects")}>
-                        {t('selection-title-projects')}
-                      </span>
-                    </a>
-                    <a>
-                      <span onClick={() => updateSectionProjects("selection-title-contact","section-text-contact")}>
-                        {t('selection-title-contact')}
-                      </span>
-                    </a>
-                  </div>
+          <h1 className='title'>{t('title')}</h1>
+          <div className='content'>
+            <div className='selector-content'>
+              <h2 className='selector-title'>{t(sectionTitle)}</h2>
+              <div className='selection-wrapper'>
+                <div className='selection'>
+                  <a>
+                    <span onClick={() => updateSection("selection-title-about-me", "lorem-ipsum-long")}>
+                      {t('selection-title-about-me')}
+                    </span>
+                  </a>
+                  <a>
+                    <span onClick={() => updateSection("selection-title-projects", "section-text-projects")}>
+                      {t('selection-title-projects')}
+                    </span>
+                  </a>
+                  <a>
+                    <span onClick={() => updateSection("selection-title-contact", "section-text-contact")}>
+                      {t('selection-title-contact')}
+                    </span>
+                  </a>
                 </div>
-              </div>       
-              {section()}     
-              <div className='spacer'/>
+              </div>
             </div>
+            <div className='spacer'/>
+            {section()}
           </div>
         </div>
         <div className='footer'>
         </div>
       </div>
-      </Suspense>
+    </Suspense>
   );
 
-  function switchContent(){
+  function switchContent() {
     return contentSwitch ? setTextContent() : setProjectContent()
   }
 
