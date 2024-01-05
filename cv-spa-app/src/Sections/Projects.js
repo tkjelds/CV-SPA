b019ba8c7ac9812ca4d8bb5ee866e06943143f01
tokj <tkjelds385@gmail.com>
2024-01-05 15:14:29 +0100

    added modal to project cards

--- cv-spa-app/src/Sections/Projects.js
+++ cv-spa-app/src/Sections/Projects.js
@@ -5,7 +5,8 @@ import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { CardActionArea } from '@mui/material';
 import profile_picture from "./img/Profile_picture_pixelart.png"
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 const ProjectsList = () => {
   const projects = [
     {
@@ -35,12 +36,31 @@ const ProjectsList = () => {
     },
 
   ]
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
   return (
     <div className='project_list'>
       {projects.map((project) => (
-        <Card sx={{ aspectRatio: 1 / 1 }}>
-          <CardActionArea>
-            <CardContent sx={{ maxHeight: 100 }}>
+        <Card sx={{ aspectRatio: 1 / 1, minHeight: 150 }}>
+          <CardActionArea onClick={handleOpen}>
+            <CardContent sx={{}}>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
@@ -51,10 +71,25 @@ const ProjectsList = () => {
             <CardMedia
               component="img"
               height={150}
-              src={profile_picture}
+              image={profile_picture}
               alt="alt-picture"
             />
           </CardActionArea>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </Card>
       ))}
     </div>
