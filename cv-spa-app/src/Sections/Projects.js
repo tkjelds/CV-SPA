b30949dba378329f5cfef618f9e97d739bbaeaf9
tokj <tkjelds385@gmail.com>
2024-01-22 11:49:30 +0100

    added styling to card, and modal. Modal needs layout justification

--- cv-spa-app/src/Sections/Projects.js
+++ cv-spa-app/src/Sections/Projects.js
@@ -1,12 +1,9 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { CardActionArea } from '@mui/material';
 import profile_picture from "./img/Profile_picture_pixelart.png"
 import Modal from '@mui/material/Modal';
-import { Height } from '@mui/icons-material';
 const ProjectsList = () => {
   const projects = [
     {
@@ -65,15 +62,15 @@ const ProjectsList = () => {
           >
             <div className='modal_box'>
               <div className='right_modal'>
-                <h1>{project.title}</h1>
+                <h1 className='modal_title'>{project.title}</h1>
                 <p>fdslkajfælksdajælfksj lskdaj fælkdsaj fælj læksdajf ælksdjælk f</p>
               </div>
               <div className='left_modal'>
                 <div className='tech_stack'>
-                  <h1>Tech stack</h1>
+                  <h1 className='modal_title'>Tech stack</h1>
                 </div>
                 <div className='github_link'>
-                  <h1>Github</h1>
+                  <h1 className=' modal_title'>Github</h1>
                 </div>
               </div>
             </div>
