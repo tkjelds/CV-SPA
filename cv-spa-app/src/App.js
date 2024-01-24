1bbe34c77f1c3eceb06d0990018e9726d3cbb81d
tkjelds <tkjelds385@gmail.com>
2024-01-24 14:41:37 +0100

    added changing gradient when selecting menu option

--- cv-spa-app/src/App.js
+++ cv-spa-app/src/App.js
@@ -4,17 +4,18 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 import useLocalStorage from "use-local-storage";
 import "./App.css";
 import Name_svg from "./Name_svg";
+import { AboutMe } from "./Sections/About_me.js";
 import { Contact } from "./Sections/Contact";
 import Projects from "./Sections/Projects";
-import { AboutMe } from "./Sections/About_me.js"
 
 function App() {
   const { t, i18n } = useTranslation();
   const [lng, setLng] = useLocalStorage("lng" ? true : false); // true = dk, false = en
   const [theme, setTheme] = useLocalStorage("theme" ? true : false); // true = light, false = dark
-  const [underlined, setUnderlined] = useState("About_me")
-  const [fade, setFade] = useState(0)
-  
+  const [underlined, setUnderlined] = useState("About_me");
+  const [gradient, setGradient] = useState("gradient1");
+  const [fade, setFade] = useState(0);
+
   const switchTheme = (checked) => {
     setTheme(checked);
   };
@@ -43,7 +44,7 @@ function App() {
         url('https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap');
       </style>
       <div className="app" data-theme={theme ? "dark" : "light"}>
-        <div className="main-container">
+        <div className="main-container" data-theme={underlined}>
           {languageSwitch()}
           <DarkModeSwitch
             className="DarkModeToggle"
@@ -61,24 +62,43 @@ function App() {
               {Name_svg()}
               <div className="menu">
                 <a onClick={() => setSection("About_me")}>
-                  <span className={underlined === "About_me" ? "underlined" : "_"}> {t("selection-title-about-me")}</span>
+                  <span
+                    className={underlined === "About_me" ? "underlined" : "_"}
+                  >
+                    {" "}
+                    {t("selection-title-about-me")}
+                  </span>
                 </a>
                 <a onClick={() => setSection("Projects")}>
-                  <span className={underlined === "Projects" ? "underlined" : "_"}>{t("selection-title-projects")}</span>
+                  <span
+                    className={underlined === "Projects" ? "underlined" : "_"}
+                  >
+                    {t("selection-title-projects")}
+                  </span>
                 </a>
                 <a onClick={() => setSection("Contacts")}>
-                  <span className={underlined === "Contacts" ? "underlined" : "_"}>{t("selection-title-contact")}</span>
+                  <span
+                    className={underlined === "Contacts" ? "underlined" : "_"}
+                  >
+                    {t("selection-title-contact")}
+                  </span>
                 </a>
               </div>
             </div>
             <div className="right">
               <div className="right_accent" />
-              <div className="right_content" onAnimationEnd={() => setFade(0)} fade={fade}>
-                {{
-                  'About_me': <AboutMe />,
-                  'Projects': <Projects />,
-                  'Contacts': <Contact />
-                }[underlined]}
+              <div
+                className="right_content"
+                onAnimationEnd={() => setFade(0)}
+                fade={fade}
+              >
+                {
+                  {
+                    About_me: <AboutMe />,
+                    Projects: <Projects />,
+                    Contacts: <Contact />,
+                  }[underlined]
+                }
               </div>
             </div>
           </div>
--- cv-spa-app/src/Sections/Projects.js
+++ cv-spa-app/src/Sections/Projects.js
@@ -1,111 +1,115 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import * as React from 'react';
-import { CardActionArea } from '@mui/material';
-import profile_picture from "./img/Profile_picture_pixelart.png"
-import Modal from '@mui/material/Modal';
-import { FaGithub } from 'react-icons/fa';
-import i18n from '../i18n';
+import { CardActionArea } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Modal from "@mui/material/Modal";
+import * as React from "react";
+import { FaGithub } from "react-icons/fa";
+import i18n from "../i18n";
+import profile_picture from "./img/Profile_picture_pixelart.png";
 
 const ProjectsList = () => {
   const projects = [
     {
       id: "mcts",
       modalLink: "https.test.com",
-      techStack: [".Net", "C#", "Xunit", "Git", "Github Actions"]
+      techStack: [".Net", "C#", "Xunit", "Git", "Github Actions"],
     },
     {
       id: "scooterSharing",
       modalLink: "https.test.com",
-      techStack: ["Kotlin", "Android studio", "JUnit", "Google firebase", "Css"]
+      techStack: [
+        "Kotlin",
+        "Android studio",
+        "JUnit",
+        "Google firebase",
+        "Css",
+      ],
     },
     {
       id: "kmd",
       modalLink: "https.test.com",
-      techStack: [".Net", "F#", "Kubernetes", "Docker", "GraphQL", "Scrum"]
+      techStack: [".Net", "F#", "Kubernetes", "Docker", "GraphQL", "Scrum"],
     },
     {
       id: "dk_map",
       modalLink: "https.test.com",
-      techStack: ["Java", "JavaFX", "Git", "XML"]
+      techStack: ["Java", "JavaFX", "Git", "XML"],
     },
     {
       id: "geocache",
       modalLink: "https.test.com",
-      techStack: ["Kotlin", "Android studio", "JUnit", "Room"]
-    },
-    {
-      id: "itforundersøgelse",
-      modalLink: "https.test.com",
-      techStack: ["MUST"]
+      techStack: ["Kotlin", "Android studio", "JUnit", "Room"],
     },
     {
       id: "spa",
       modalLink: "https.test.com",
-      techStack: ["React", "Javascript", "Css", "I18n", "Mui"]
+      techStack: ["React", "Javascript", "Css", "I18n", "Mui"],
     },
-
-  ]
+  ];
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = (clickedProject) => {
     setOpen(true);
     setSelectedProject(clickedProject);
-  }
+  };
   const handleClose = () => setOpen(false);
   const [selectedProject, setSelectedProject] = React.useState({
     id: "null",
     modalLink: "null",
-    techStack: ["null"]
+    techStack: ["null"],
   });
 
   return (
-    <div className='project_list'>
+    <div className="project_list">
       {projects.map((project) => (
-        <div className='card'>
+        <div className="card">
           <Card sx={{ backgroundColor: "white" }}>
-            <CardActionArea onClick={() => handleOpen(project)} sx={{ aspectRatio: 1 / 1 }}>
+            <CardActionArea
+              onClick={() => handleOpen(project)}
+              sx={{ aspectRatio: 1 / 1 }}
+            >
               <CardContent>
-
-                <h2 className='card_title'>
+                <h2 className="card_title">
                   {i18n.t("section.projects." + project.id + ".title")}
                 </h2>
-                <p className='card_desc'>
-                  {i18n.t("section.projects." + project.id + ".card_description")}
+                <p className="card_desc">
+                  {i18n.t(
+                    "section.projects." + project.id + ".card_description"
+                  )}
                 </p>
-
               </CardContent>
             </CardActionArea>
           </Card>
         </div>
-      ))
-      }<Modal
-        open={open}
-        onClose={handleClose}
-      >
-        <div className='modal_box'>
-          <div className='right_modal'>
-            <h1 className='modal_title'>{i18n.t("section.projects." + selectedProject.id + ".title")}</h1>
-            {i18n.t("section.projects." + selectedProject.id + ".modal_description")}
+      ))}
+      <Modal open={open} onClose={handleClose}>
+        <div className="modal_box">
+          <div className="right_modal">
+            <h1 className="modal_title">
+              {i18n.t("section.projects." + selectedProject.id + ".title")}
+            </h1>
+            {i18n.t(
+              "section.projects." + selectedProject.id + ".modal_description"
+            )}
           </div>
-          <div className='left_modal'>
-            <div className='tech_stack'>
-              <h1 className='modal_title'>Tech stack</h1>
-              <ul className='tech_stack_modal'>
+          <div className="left_modal">
+            <div className="tech_stack">
+              <h1 className="modal_title">Tech stack</h1>
+              <ul className="tech_stack_modal">
                 {selectedProject.techStack.map((tech) => (
                   <li> {tech}</li>
                 ))}
               </ul>
             </div>
-            <div className='github_link'>
-              <a href={selectedProject.modalLink} className='modal_icon'>
+            <div className="github_link">
+              <a href={selectedProject.modalLink} className="modal_icon">
                 {<FaGithub />}
               </a>
             </div>
           </div>
         </div>
       </Modal>
-    </div >
+    </div>
   );
 };
 
